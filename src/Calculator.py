fd974aeed18949c72373b131f8d762a0d8fe4184
vidhipatel <vkp8.njit.edu>
2021-06-27 20:25:39 -0400

    Feature : Setup calculator with subtration

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,16 +1,21 @@
 def addition (a, b):
     return a+b
 
+def subtaction (a, b):
+    return a-b
+
 
 
 class Calculator:
     result = 0
 
     def __init__(self):
-        x=2+2
-        self.result = x;
         pass
 
     def add(self, a, b):
-        self.result = a+b
-        return addition(a, b)
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtaction(a, b)
+        return self.result
