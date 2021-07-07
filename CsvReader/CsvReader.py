de1ca700b4d3a9eefa7dc85a6cbcce6b6bbb6242
vidhipatel <vkp8.njit.edu>
2021-07-07 15:56:16 -0400

    Update csvreaderfile

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -2,22 +2,22 @@ import csv
 from pprint import pprint
 
 
-#def ClassFactory(class_name, dictionary):
-   # return type(class_name, (object,), dictionary)
+def ClassFactory(class_name, dictionary):
+    return type(class_name, (object,), dictionary)
 
 #Read csv file
 class CsvReader:
+    data = []
 
     def __init__(self, filepath):
         self.data = []
-        with open(filepath) as text_data:
-            csv_data = csv.DictReader(text_data, delimiter=',')
-            for row in csv_data:
-                self.data.append(row)
-
-    @property
-    def get_data(self):
-        return self.data
+        try:
+            with open(filepath) as text_data:
+                csv_data = csv.DictReader(text_data, delimiter=',')
+                for row in csv_data:
+                    self.data.append(row)
+        except OSError:
+            print('cannot open', filepath)
 
     def return_data_as_objects(self, class_name):
         objects = []
